f921a9b29354eb0e29a6ffb3ba4acf5bd635a78a
Hampus Rosvall <hampusrosvall@hr.lan>
2020-05-26 10:01:48 +0200

    fixed some bugs in DFS

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -11,6 +11,10 @@ const SOURCE_COL = 5;
 const TARGET_ROW = 10;
 const TARGET_COL = 38;
 
+const STANDARD_COLOR = "whitesmoke";
+const SOURCE_COLOR = "lightgreen";
+const TARGET_COLOR = "lightcoral";
+
 export default class PathFindingVisualizer extends React.Component {
   constructor(props) {
     super(props);
@@ -38,8 +42,16 @@ export default class PathFindingVisualizer extends React.Component {
   }
 
   reset() {
-    const grid = resetGrid();
-    this.setState({ grid: grid });
+    const grid = this.state.grid;
+    for (const row of grid) {
+      for (const node of row) {
+        node.sourceNode
+          ? markNode(node, SOURCE_COLOR)
+          : node.targetNode
+          ? markNode(node, TARGET_COLOR)
+          : markNode(node, STANDARD_COLOR);
+      }
+    }
   }
 
   async BFS() {
@@ -130,7 +142,7 @@ export default class PathFindingVisualizer extends React.Component {
 }
 
 async function markNode(node, color) {
-  await sleep(10);
+  await sleep(1);
   const row = node.row;
   const col = node.col;
   const visitedNodeStyle = document.getElementById(`${row}-${col}`).style;
@@ -139,22 +151,4 @@ async function markNode(node, color) {
 
 function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-function resetGrid() {
-  const grid = [];
-  for (let row = 0; row < ROWS; row++) {
-    let currentRow = [];
-    for (let col = 0; col < COLS; col++) {
-      const currentNode = {
-        row: row,
-        col: col,
-        sourceNode: row === SOURCE_ROW && col === SOURCE_COL,
-        targetNode: row === TARGET_ROW && col === TARGET_COL,
-      };
-      currentRow.push(currentNode);
-    }
-    grid.push(currentRow);
-  }
-  return grid;
 }
